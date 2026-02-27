3b5f5b5ef42de57bdc758ec3685c8a30620e0c35
Michael Stevens <mich8060@hotmail.com>
2026-02-26 18:15:23 -0700

    Making the App Shell included in the npm package.

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -5,3 +5,12 @@ export * from "./components/Icon";
 export * from "./components/TextInput";
 export { default as Toggle } from "./components/Toggle/Toggle";
 export type { ToggleProps } from "./components/Toggle/Toggle.types";
+export { default as Menu } from "./components/Menu/Menu";
+export type { MenuProps } from "./components/Menu/Menu.types";
+export { AppShell } from "../app-shell/AppShell";
+export type {
+  ShellLayoutConfig,
+  ContainerMode,
+  DensityMode,
+  PaddingMode,
+} from "../app-shell/layout.types";
